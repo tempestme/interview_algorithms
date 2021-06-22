7793f5f998fd96ef4bc233df1a9c877252753c98
Pavel <paulsecondtwin@gmail.com>
2021-06-22 19:42:36 +0300

    leetcode twosum challenge solution

---
+++ src/leetcode_twosum.kt
@@ -0,0 +1,18 @@
+fun main(){
+    foundSum(6, arrayOf(3,2,4))
+}
+
+fun foundSum(target:Int, nums:Array<Int>):IntArray{
+    val set = nums.toSet()
+    nums.map {  }
+
+    nums.forEachIndexed(){ index: Int, integer: Int ->
+        if (set.contains(target-integer)){
+            nums.forEachIndexed(){ index2: Int, integer2: Int ->
+                println("$integer $integer2")
+                if (integer+integer2==target&&index!=index2)return intArrayOf(index,index2)
+            }
+        }
+    }
+    return intArrayOf()
+}
