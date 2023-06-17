8f58ef5db500f454fdeb43d0e3d552942655010c
pavel <pyakunin@eyeconweb.com>
2023-06-18 01:12:54 +0300

    242 Valid_anagram solved with 2 different solutions.

--- src/leetcode/217.Contains_Duplicate.kt
+++ src/leetcode/217.Contains_Duplicate.kt
@@ -6,10 +6,8 @@ package leetcode
  * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
  */
 
-class Solution {
-    fun containsDuplicate(nums: IntArray): Boolean {
+ fun containsDuplicate(nums: IntArray): Boolean {
         val seen = HashSet<Int>()
         nums.forEach { if (!seen.add(it)) return true }
         return false
     }
-}
